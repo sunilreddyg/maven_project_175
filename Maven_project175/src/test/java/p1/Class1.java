af18f8c22038d9c430081037c8659ee0e07fe749
Administrator <Administrator@AA-PC>
2020-01-29 10:19:05 +0530

    Firstproject

---
+++ Maven_project175/src/test/java/p1/Class1.java
@@ -0,0 +1,23 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+  @Test
+  public void tc001() 
+  {
+	  System.out.println("tc001 executed");
+  }
+  
+  @Test
+  public void tc002() 
+  {
+	  System.out.println("tc002 executed");
+  }
+  
+  @Test
+  public void tc003() 
+  {
+	  System.out.println("tc003 executed");
+  }
+}
---
+++ Maven_project175/src/test/java/p2/Class2.java
@@ -0,0 +1,25 @@
+package p2;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+  
+	 @Test
+	  public void tc004() 
+	  {
+		  System.out.println("tc004 executed");
+	  }
+	  
+	  @Test
+	  public void tc005() 
+	  {
+		  System.out.println("tc005 executed");
+	  }
+
+	  @Test
+	  public void tc006() 
+	  {
+		  System.out.println("tc006 executed");
+	  }
+	  
+}
